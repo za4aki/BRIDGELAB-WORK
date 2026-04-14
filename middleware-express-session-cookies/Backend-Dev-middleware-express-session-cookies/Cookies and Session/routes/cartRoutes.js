ee2708b893845259ae1bbfe6058c60c8d33122de
Kartikey Sharma <kartikey90gb@gmail.com>
2026-04-15 00:35:46 +0530

    Delete middleware-express-session-cookies/Backend-Dev-middleware-express-session-cookies directory

--- middleware-express-session-cookies/Backend-Dev-middleware-express-session-cookies/Cookies and Session/routes/cartRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const cartController = require("../features/cartSystem");
-
-router.use("/", cartController);
-
-module.exports = router;
--- middleware-express-session-cookies/Backend-Dev-middleware-express-session-cookies/Cookies and Session/routes/langRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const langController = require("../features/languagePreference");
-
-router.use("/", langController);
-
-module.exports = router;
