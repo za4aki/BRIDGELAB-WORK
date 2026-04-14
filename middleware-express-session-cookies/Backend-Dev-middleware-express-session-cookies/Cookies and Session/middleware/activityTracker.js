473ed99193e9fbe7abf2fb8f81c39f838e29bd4e
Kartikey Sharma <kartikey90gb@gmail.com>
2026-04-15 00:43:29 +0530

    Delete middleware-express-session-cookies/Backend-Dev-middleware-express-session-cookies directory

--- middleware-express-session-cookies/Backend-Dev-middleware-express-session-cookies/Cookies and Session/middleware/activityTracker.js
+++
@@ -1,6 +0,0 @@
-const activityTracker = (req, res, next) => {
-  req.session.lastActivity = Date.now();
-  next();
-};
-
-module.exports = activityTracker;
--- middleware-express-session-cookies/Backend-Dev-middleware-express-session-cookies/Cookies and Session/routes/sessionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { router: sessionController } = require("../features/sessionTimeout");
-
-router.use("/", sessionController);
-
-module.exports = router;
